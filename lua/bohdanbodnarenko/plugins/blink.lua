b3455d74d47fb62c386bfd96595caabf76bd85c5
Bohdan Bodnarenko <bohdan.bodnarenko@vista.com>
2024-11-28 10:00:23 +0200

    added blink plugin

---
+++ lua/bohdanbodnarenko/plugins/blink.lua
@@ -0,0 +1,25 @@
+return {
+  'saghen/blink.cmp',
+  conf = {
+    lazy = false,
+    dependencies = 'rafamadriz/friendly-snippets',
+    opts = {
+      keymap = {
+        preset = 'enter',
+      },
+      highlight = {
+        use_nvim_cmp_as_default = true,
+      },
+      windows = {
+        autocomplete = {
+          draw = {
+            columns = { { 'label', 'label_description', gap = 1 }, { 'kind_icon', 'kind' } },
+          },
+        },
+      },
+      nerd_font_variant = 'normal',
+      accept = { auto_brackets = { enabled = true } },
+      trigger = { signature_help = { enabled = true } },
+    },
+  },
+}
