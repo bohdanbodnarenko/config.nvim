7fb83eb44f562c49c8a36897579fd85dd39a64c5
Bohdan Bodnarenko <bohdan.bodnarenko@vista.com>
2024-11-28 09:59:19 +0200

    updated configs

--- lua/bohdanbodnarenko/plugins/colorsheme.lua
+++ lua/bohdanbodnarenko/plugins/colorsheme.lua
@@ -1,18 +1,51 @@
-return { -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is.
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+return {
   'rebelot/kanagawa.nvim',
   lazy = false,
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
     vim.cmd.colorscheme 'kanagawa'
 
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
+    -- vim.cmdhi 'Comment gui=none'
+  end,
+  config = function()
+    require('kanagawa').setup {
+      compile = true,
+      dimInactive = true,
+      overrides = function(colors)
+        local theme = colors.theme
+        local palette = colors.palette
+        return {
+
+          IndentBlanklineChar = { fg = palette.waveBlue2 },
+          MiniIndentscopeSymbol = { fg = palette.waveBlue2 },
+          PmenuSel = { blend = 0 },
+          NormalFloat = { bg = 'none' },
+          FloatBorder = { bg = 'none' },
+          FloatTitle = { bg = 'none' },
+          CursorLineNr = { bg = theme.ui.bg_p2 },
+          Visual = { bg = palette.waveBlue2 },
+
+          -- Save an hlgroup with dark background and dimmed foreground
+          -- so that you can use it where your still want darker windows.
+          -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+          -- Popular plugins that open floats will link to NormalFloat by default;
+          -- set their background accordingly if you wish to keep them dark and borderless
+          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+        }
+      end,
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = 'none',
+            },
+          },
+        },
+      },
+    }
+
+    require('kanagawa').load 'wave'
   end,
 }
--- lua/bohdanbodnarenko/plugins/which-key.lua
+++ lua/bohdanbodnarenko/plugins/which-key.lua
@@ -2,7 +2,23 @@ return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
   opts = {
+    -- preset = 'helix',
+    delay = 0,
+    win = {
+      height = {
+        max = math.huge,
+      },
+    },
+    plugins = {
+      spelling = {
+        enabled = false,
+      },
+    },
     icons = {
+      rules = false,
+      breadcrumb = ' ', -- symbol used in the command line area that shows your active key combo
+      separator = '󱦰  ', -- symbol used between a key and it's label
+      group = '󰹍 ', -- symbol prepended to a group
       -- set icon mappings to true if you have a Nerd Font
       mappings = vim.g.have_nerd_font,
       -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
