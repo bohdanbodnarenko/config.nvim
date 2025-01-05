20bc98034a39fd9efe2ae138b537ca2ecdb234b4
Bohdan Bodnarenko <bohdan.bodnarenko@vista.com>
2025-01-05 14:31:59 +0200

    added new autocommands

--- lua/bohdanbodnarenko/core/autocommands.lua
+++ lua/bohdanbodnarenko/core/autocommands.lua
@@ -9,12 +9,37 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- Run gofmt + goimport on save
-local format_sync_grp = vim.api.nvim_create_augroup('GoImport', {})
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = '*.go',
-  callback = function()
-    require('go.format').goimport()
+-- Prevent LSP from overwriting treesitter color settings
+-- https://github.com/NvChad/NvChad/issues/1907
+vim.highlight.priorities.semantic_tokens = 95 -- Or any number lower than 100, treesitter's priority level
+
+-- Appearance of diagnostics
+vim.diagnostic.config {
+  virtual_text = {
+    prefix = '●',
+    -- Add a custom format function to show error codes
+    format = function(diagnostic)
+      local code = diagnostic.code and string.format('[%s]', diagnostic.code) or ''
+      return string.format('%s %s', code, diagnostic.message)
+    end,
+  },
+  underline = false,
+  update_in_insert = true,
+  float = {
+    source = true,
+  },
+  -- Make diagnostic background transparent
+  on_ready = function()
+    vim.cmd 'highlight DiagnosticVirtualText guibg=NONE'
   end,
-  group = format_sync_grp,
-})
+}
+
+-- Run gofmt + goimport on save
+-- local format_sync_grp = vim.api.nvim_create_augroup('GoImport', {})
+-- vim.api.nvim_create_autocmd('BufWritePre', {
+--   pattern = '*.go',
+--   callback = function()
+--     require('go.format').goimport()
+--   end,
+--   group = format_sync_grp,
+-- })
